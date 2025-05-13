89985487fdf009a8ca8a7768b52ce8ea843036df
DeeIII <111374536+DeeIII@users.noreply.github.com>
2025-05-13 12:59:17 +0100

    Add files via upload

---
+++ weight_converter.py
@@ -0,0 +1,21 @@
+"""This module converts weights from kilogramms to poundsza"""
+
+
+def weight_converter(user_weight):
+    """Return weight in pounds given a kilogram input"""
+
+    try:
+        weight_lbs = float(user_weight) / 0.45
+        return weight_lbs
+    except ValueError:
+        print('that was not a valid number')
+
+
+def weight_converter_v2():
+    """Return weight in pounds given a kilogram input"""
+
+    try:
+        weight_lbs = float(input("Enter your weight in kg: ")) / 0.45
+        return weight_lbs
+    except ValueError:
+        print('that was not a valid number')
